6e5a2203518e0855d268d6e8dae72d1dff839d2e
zhanglei <zhanglei@192.168.100.19>
2016-06-02 23:19:55 +0800

    修改获取截屏的核心方法

--- CutterTest/Cutter/UIView+Cutter.m
+++ CutterTest/Cutter/UIView+Cutter.m
@@ -14,11 +14,17 @@
  *  根据视图尺寸获取视图截屏
  *
  *  @return UIImage 截取的图片
+ *  参考：https://developer.apple.com/library/ios/qa/qa1817/_index.html#//apple_ref/doc/uid/DTS40014134
  */
 - (UIImage*)viewCutter
 {
     UIGraphicsBeginImageContextWithOptions(self.bounds.size,NO,[[UIScreen mainScreen] scale]);
-    [self.layer renderInContext:UIGraphicsGetCurrentContext()];
+    
+    // 方法一 有时导航条无法正常获取
+    // [self.layer renderInContext:UIGraphicsGetCurrentContext()];
+    // 方法二 iOS7.0 后推荐使用
+     [self drawViewHierarchyInRect:self.bounds afterScreenUpdates:YES];
+    
     UIImage*img = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
     
